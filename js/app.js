9f415f8b71d1f3e600ac4fdb4ebb5cd24050c280
Piyush Waradpande <pi5@umbc.edu>
2015-06-08 17:04:49 -0400

    Open new entry modal by pressing 'n' key.

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ var promiseApp = angular.module('promiseApp', []);
 
 promiseApp.controller('MainController', function ($scope) {
     $scope.name = "PROMISE";
+    $scope.openModal = function(event) {
+
+        /* Open new entry dialog on pressing the 'n' key */
+        if (event && event.keyCode == 78) {
+            $('#newentry-button').click();
+        }
+    };
 });
